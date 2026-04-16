7bcf3984929aebf5746402e11cfe65c38d512ace
EduardoBertinettiTorres <eduardotorres.pl012@academico.ifsul.edu.br>
2026-04-16 16:27:44 -0300

    deploy

--- apps/portal-api/src/app/app.module.ts
+++ apps/portal-api/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,26 +8,27 @@ import { Project } from './project.entity';
 import { Milestone } from './milestone.entity';
 import { Comment } from './comment.entity';
 import { AuthModule } from './auth/auth.module';
-
-// Importe o módulo recém-criado
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
-import { MilestonesModule } from './milestones/milestones.module'
+import { MilestonesModule } from './milestones/milestones.module';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'admin',
-      password: 'adminpassword',
-      database: 'portal_db',
-      entities: [User, Project, Milestone, Comment],
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
     }),
-    // Avise o NestJS que o módulo de usuários existe!
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [User, Project, Milestone, Comment],
+        synchronize: false,
+      }),
+    }),
     UsersModule,
     ProjectsModule,
     MilestonesModule,
